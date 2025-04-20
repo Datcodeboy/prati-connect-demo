1dc7c06ebac7777f01d09a82979567ccbffdaa4f
Datcodeboy <datcodeboy@null.net>
2025-04-20 16:25:02 -0300

    Updated HTML with language selector and feedback form

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'prati-connect-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://raichu-uploads.s3.amazonaws.com/logo_prati-donaduzzi_I0eXhm.jpg'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("prati-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
