8d01d9dff4fbd407a62d0965e63fa16fbdfdab5e
Datcodeboy <datcodeboy@null.net>
2025-04-21 20:01:15 -0300

    Added advanced features to Prati-Connect: 7 tools + flags

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'prati-connect-cache-v1';
+const CACHE_NAME = 'prati-connect-cache-v2';
+
 const urlsToCache = [
   './',
   './index.html',
@@ -7,16 +8,16 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
-
